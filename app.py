b8a9f71799d698c77eca180d5e131962f6ad9b3e
raquelcoelho <raquelsantos.coelho@gmail.com>
2023-09-28 21:42:55 -0300

    Adicionado função principal

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import streamlit as st
 def load_data():
     return pd.read_csv('data/processed/bikes_completed.csv')
 
-df = load_data()
-
-st.dataframe(df)
+def main():
+    df = load_data()
+    st.dataframe(df)
+    
+if __name__ == '__main__':
+    main()
